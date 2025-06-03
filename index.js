2a016c2364e30434cd6f82bd18938238bce619b9
XiLeF2211 <xilefthenerd@gmail.com>
2025-06-02 21:43:23 -0700

    Add search and filter functionality and additional symbols

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const proxyURL = 'https://api.codetabs.com/v1/proxy/?quest='
 const mapURL = 'https://map.earthmc.net/tiles'
-const highwaysURL = 'https://raw.githubusercontent.com/XiLeF2211/Cosmic-Reach-Localization/eab6f86f841f6bb2819639b31e0f2926177dd2d4/assets/base/highways.json'
+const highwaysURL = 'https://raw.githubusercontent.com/XiLeF2211/ice-highways-map/refs/heads/main/highways.json'
 
 let highwayData
 
-const towns = []
-const townLabels = []
+let marker = L.marker([0, 0])
 
 let viewHistory = []
-
 let selectedStation
+let stationFilters = {
+    contains: '',
+    type: ['station', 'semi', 'jct', 'inter']
+}
+let lineFilters = {
+    contains: '',
+    company: []
+}
 
 const map = L.map('mapa', {
     crs: L.CRS.Simple,
@@ -33,11 +39,13 @@ async function init() {
     if (!highwayData) {
         console.log('debug: There was a problem with getting station and line data')
     }
+    lineFilterOpts();
+    listLine()
+    listStation();
+
     await renderTowns();
     renderLines();
     renderStations();
-    listLine()
-    listStation();
 }
 
 async function renderTowns() {
@@ -47,16 +55,16 @@ async function renderTowns() {
         console.log('debug: There was a problem with getting towns data')
         return
     }
-    
+
     const regions = []
-    
+
     // Collect info about towns
     for (const town of data[0].markers) {
         if (town.type != 'polygon') continue
-        
+
         const townName = town.tooltip.match(/<b>(.*)<\/b>/)[1]
         const nation = town.tooltip.match(/\(\b(?:Member|Capital)\b of (.*)\)\n/)?.at(1)
-        
+
         // Might need capital? maybe
         let isCapital = town.tooltip.search(/\(Capital of /)
         const fill = town.fillColor
@@ -66,7 +74,7 @@ async function renderTowns() {
             for (const vertex of region[0]) {
                 vertices.push([-vertex.z / 16, vertex.x / 16])
             }
-            
+
             if (regions.find(region => region.town == townName)) isCapital = false
             regions.push({
                 town: townName,
@@ -78,7 +86,7 @@ async function renderTowns() {
             })
         }
     }
-    
+
     // Place down every collected town
     for (const region of regions) {
         L.polygon(region.vertices, {
@@ -87,7 +95,7 @@ async function renderTowns() {
             weight: 1
         }).addTo(map).bindPopup(region.town + (region.nation != undefined ? ', ' + region.nation : ''))
     }
-    
+
     const stopTownRender = new Date()
     const diff = stopTownRender - startTownRender
     console.log(`debug: Rendering towns took ${diff}ms`)
@@ -95,44 +103,43 @@ async function renderTowns() {
 
 async function renderLines() {
     const startLineRender = new Date()
-    
+
     for (const company of Object.keys(highwayData.lines)) {
         for (const line of Object.keys(highwayData.lines[company])) {
             // Modify vertices
-            const vertices = []
-            for (const branch of highwayData.lines[company][line].vertices) {
-                vertices.push([])
-                for (const vertex of branch) {
-                    vertices[vertices.length - 1].push([-vertex[1] / 16, vertex[0] / 16])
+            for (const branch of Object.keys(highwayData.lines[company][line].vertices)) {
+                const vertices = []
+                for (const vertex of highwayData.lines[company][line].vertices[branch]) {
+                    vertices.push([-vertex[1] / 16, vertex[0] / 16])
                 }
+
+                let container = document.createElement('div');
+                let lineName = document.createElement('a')
+                lineName.innerHTML = `${line} line <em>${branch != '' ? `(${branch})` : ''}</em>`
+                lineName.style.cursor = 'pointer'
+                lineName.addEventListener('click', () => {
+                    openSide();
+                    showLine(company, line, highwayData.lines[company][line]);
+                    viewHistory.push(['line', company, line, highwayData.lines[company][line]])
+                })
+                container.appendChild(lineName)
+                L.polyline(vertices, {
+                    color: '#' + highwayData.lines[company][line].color,
+                    weight: 5
+                })
+                    .addTo(map)
+                    .bindPopup(container)
             }
-            
-            let container = document.createElement('div');
-            let lineName = document.createElement('a')
-            lineName.innerHTML = line
-            lineName.style.cursor = 'pointer'
-            lineName.addEventListener('click', () => {
-                openSide();
-                showLine(company, line, highwayData.lines[company][line]);
-                viewHistory.push(['line', company, line, highwayData.lines[company][line]])
-            })
-            container.appendChild(lineName)
-            L.polyline(vertices, {
-                color: '#' + highwayData.lines[company][line].color,
-                weight: 5
-            })
-            .addTo(map)
-            .bindPopup(container)
         }
     }
-    
+
     const stopLineRender = new Date()
     console.log(`debug: Rendering lines took ${stopLineRender - startLineRender}ms`)
 }
 
 async function renderStations() {
     const startStationRender = new Date()
-    
+
     for (const station of highwayData.stations) {
         let container = document.createElement('div');
         let stationName = document.createElement('a')
@@ -144,88 +151,33 @@ async function renderStations() {
             viewHistory.push(['station', station])
         })
         container.appendChild(stationName)
-        
-        L.circleMarker([-station.z / 16, station.x / 16], {
+
+        if (Object.hasOwn(station, 'type')) {
+            L.marker([-station.z / 16, station.x / 16], {
+                icon: L.icon({
+                    iconUrl: 'assets/symbols/' + station.type + '.svg',
+                    iconSize: 14
+                })
+            }).addTo(map).bindPopup(container)
+        } else L.circleMarker([-station.z / 16, station.x / 16], {
             radius: 5,
             color: '#ffffff'
         })
-        .addTo(map)
-        .bindPopup(container)
+            .addTo(map)
+            .bindPopup(container)
     }
-    
+
     const stopStationRender = new Date()
     console.log(`debug: Rendering stations took ${stopStationRender - startStationRender}ms`)
-}
-
-function listLine() {
-    for (const company of Object.keys(highwayData.lines)) {
-        for (const line of Object.keys(highwayData.lines[company])) {
-            let lineItem = document.createElement('dt')
-            let lineName = document.createElement('a')
-            lineName.innerHTML = `${line} (${company})`
-            lineItem.style.cursor = 'pointer'
-            lineItem.addEventListener('click', () => {
-                showLine(company, line, highwayData.lines[company][line])
-                viewHistory.push(['line', company, line, highwayData.lines[company][line]])
-            })
-            lineItem.appendChild(lineName)
-            document.getElementById('line-list').appendChild(lineItem)
-        }
-    }
-}
-
-function listStation() {
-    for (const station of highwayData.stations) {
-        let stationItem = document.createElement('dt')
-        let stationName = document.createElement('a')
-        stationName.innerHTML = station.name
-        stationItem.style.cursor = 'pointer'
-        stationItem.addEventListener('click', () => {
-            showStation(station)
-            viewHistory.push(['station', station])
-        })
-        stationItem.appendChild(stationName)
-        document.getElementById('station-list').appendChild(stationItem)
-    }
-}
-
-function getArea(vertices) {
-    const n = vertices.length
-    let area = 0
-    
-    // Vertices need rounding to 16 because data has imprecise coordinates
-    for (let i = 0; i < n; i++) {
-        const j = (i + 1) % n
-        area += roundTo16(vertices[i].x) * roundTo16(vertices[j].z)
-        area -= roundTo16(vertices[j].x) * roundTo16(vertices[i].z)
-    }
-    
-    return (Math.abs(area) / 2) / (16 * 16)
-}
-
-async function fetchJSON(url) {
-    const response = await fetch(url)
-    if (response.ok) return response.json()
-        else return null
-}
-
-function roundTo16(number) {
-    return Math.round(number / 16) * 16
-}
-
-function openSide() {
-    document.getElementById('sidebar').style.transform = 'translateX(0%)'
-}
-
-function closeSide() {
-    document.getElementById('sidebar').style.transform = 'translateX(-200%)'
 }
 
 function showLine(companyName, lineName, line) {
     goToTab('line')
     goToDetails('line')
-    
+
     document.getElementById('line-name').textContent = companyName + ': ' + lineName + ' line'
+    if (line.code.length > 0) document.getElementById('line-code').textContent = `Code: ${line.code}`
+    else document.getElementById('line-code').textContent = ''
     document.getElementById('line-y').textContent = `y-level (ice block level) at ${line.y}`
     document.getElementById('line-stations').innerHTML = ''
     for (const station of line.stations) {
@@ -234,17 +186,17 @@ function showLine(companyName, lineName, line) {
             showStation(highwayData.stations[station])
             viewHistory.push(['station', highwayData.stations[station]])
         })
-        stations.innerHTML = `${line.code}${highwayData.stations[station].lines[companyName][lineName]} ${highwayData.stations[station].name}<br>`
-        document.getElementById('line-stations').appendChild(stations)    
+        stations.innerHTML = `${line.prefix}${highwayData.stations[station].lines[companyName][lineName]} ${highwayData.stations[station].name}<br>`
+        document.getElementById('line-stations').appendChild(stations)
     }
 }
 
 function showStation(station) {
     goToTab('station')
     goToDetails('station')
-    
+
     document.getElementById('station-name').textContent = station.name
-    document.getElementById('station-location').textContent = `X: ${station.x} Z: ${station.z}`
+    document.getElementById('station-location').innerHTML = `X: ${station.x} Z: ${station.z} <a onclick="locate(${station.x}, ${station.z})">Locate</a>`
     document.getElementById('station-codes').innerHTML = ''
     for (company in station.lines) {
         let companyName = document.createElement('h3')
@@ -258,10 +210,106 @@ function showStation(station) {
                 viewHistory.push(['line', company, line, highwayData.lines[company][line]])
             })
             lineLink.innerHTML = `(${line} line)`
-            code.innerHTML = `${highwayData.lines[company][line].code}${station.lines[company][line]} `
+            code.innerHTML = `${highwayData.lines[company][line].prefix}${station.lines[company][line]} `
             code.appendChild(lineLink)
             document.getElementById('station-codes').appendChild(code)
         }
+    }
+}
+
+function listLine() {
+    document.getElementById('line-list').replaceChildren()
+    for (const company of Object.keys(highwayData.lines)) {
+        if (!lineFilters.company.includes(company)) continue
+
+        for (const line of Object.keys(highwayData.lines[company])) {
+            if (line.toUpperCase().indexOf(lineFilters.contains) < 0) continue
+
+            let lineItem = document.createElement('dt')
+            let lineName = document.createElement('a')
+            lineName.innerHTML = `${line} line (${company})`
+            lineItem.style.cursor = 'pointer'
+            lineItem.addEventListener('click', () => {
+                showLine(company, line, highwayData.lines[company][line])
+                viewHistory.push(['line', company, line, highwayData.lines[company][line]])
+            })
+            lineItem.appendChild(lineName)
+            document.getElementById('line-list').appendChild(lineItem)
+        }
+    }
+}
+
+function listStation() {
+    document.getElementById('station-list').replaceChildren()
+    for (const station of highwayData.stations) {
+        if (station.name.toUpperCase().indexOf(stationFilters.contains) < 0) continue
+        if (!stationFilters.type.includes(station.type == undefined ? 'station' : station.type)) continue
+
+        let stationItem = document.createElement('dt')
+        let stationName = document.createElement('a')
+        stationName.innerHTML = station.name
+        stationItem.style.cursor = 'pointer'
+        stationItem.addEventListener('click', () => {
+            showStation(station)
+            viewHistory.push(['station', station])
+        })
+        stationItem.appendChild(stationName)
+        document.getElementById('station-list').appendChild(stationItem)
+    }
+}
+
+function filterLines() {
+    lineFilters = {
+        contains: document.getElementById('search-line').value.toUpperCase(),
+        company: checkFilters('line-filter-company')
+    }
+    listLine()
+}
+
+function filterStations() {
+    stationFilters = {
+        contains: document.getElementById('search-station').value.toUpperCase(),
+        type: checkFilters('station-filter-type')
+    }
+    listStation()
+}
+
+function lineFilterOpts() {
+    for (const company of Object.keys(highwayData.lines)) {
+        let companyInput = document.createElement('input')
+        let companyLabel = document.createElement('label')
+        companyInput.type = 'checkbox'
+        companyInput.id = 'company-' + company
+        companyInput.value = company
+        companyInput.checked = true
+        companyLabel.innerHTML = company
+        companyLabel.htmlFor = 'company-' + company
+
+        let temp = document.createElement('div')
+        temp.appendChild(companyInput)
+        temp.appendChild(companyLabel)
+        document.getElementById('line-filter-company').appendChild(temp)
+
+        lineFilters.company.push(company)
+    }
+}
+
+function checkFilters(filterId) {
+    let chosen = []
+    document.querySelectorAll(`#${filterId} input[type="checkbox"]`).forEach(option => {
+        if (option.checked) chosen.push(option.value)
+    })
+    return chosen
+}
+
+function openFilter(filterName) {
+    document.getElementById('filters').style.zIndex = 100
+    if (filterName == 'station') {
+        document.getElementById('station-filter-container').style.display = 'block'
+        document.getElementById('line-filter-container').style.display = 'none'
+    } else {
+        document.getElementById('station-filter-container').style.display = 'none'
+        document.getElementById('line-filter-container').style.display = 'block'
     }
 }
 
@@ -270,7 +318,7 @@ function goToTab(tab) {
     document.getElementById('line').style.display = tab == 'line' ? 'block' : 'none'
     document.getElementById('station').style.display = tab == 'station' ? 'block' : 'none'
     document.getElementById('settings').style.display = tab == 'settings' ? 'block' : 'none'
-    
+
     tab == 'main' ? document.getElementById('about-tab').classList.add('active') : document.getElementById('about-tab').classList.remove('active')
     tab == 'line' ? document.getElementById('line-tab').classList.add('active') : document.getElementById('line-tab').classList.remove('active')
     tab == 'station' ? document.getElementById('station-tab').classList.add('active') : document.getElementById('station-tab').classList.remove('active')
@@ -278,7 +326,7 @@ function goToTab(tab) {
 }
 
 function goToList(tab) {
-    document.getElementById(tab + '-list-container').style.display = 'block'
+    document.getElementById(tab + '-list-container').style.display = 'flex'
     document.getElementById(tab + '-details').style.display = 'none'
 }
 
@@ -288,6 +336,12 @@ function goToDetails(tab) {
 }
 
 function goBackHistory() {
+    if (viewHistory.length <= 1) {
+        goToTab(viewHistory[0][0] == 'station' ? 'station' : 'line')
+        goToList(viewHistory[0][0] == 'station' ? 'station' : 'line')
+        viewHistory.pop()
+        return
+    }
     const temp = viewHistory[viewHistory.length - 2]
     if (temp[0] == 'station') {
         showStation(temp[1])
@@ -302,6 +356,33 @@ function goBackHistory() {
     }
     viewHistory.pop();
 }
+
+function openSide() {
+    document.getElementById('sidebar').style.transform = 'translateX(0%)'
+}
+
+function closeSide() {
+    document.getElementById('sidebar').style.transform = 'translateX(-200%)'
+}
+
+function locate(x, z) {
+    map.panTo([-z / 16, x / 16])
+    marker = L.marker([-z / 16, x / 16]).addTo(map)
+}
+
+async function fetchJSON(url) {
+    const response = await fetch(url)
+    if (response.ok) return response.json()
+    else return null
+}
+
+function roundTo16(number) {
+    return Math.round(number / 16) * 16
+}
+
+document.getElementById('mapa').addEventListener('click', () => {
+    marker.remove()
+})
 
 document.getElementById('about-tab').addEventListener('click', () => {
     goToTab('main')
@@ -321,4 +402,8 @@ document.getElementById('line-tab').addEventListener('click', () => {
 
 document.getElementById('settings-tab').addEventListener('click', () => {
     goToTab('settings')
+})
+
+window.addEventListener('click', (e) => {
+    if (!document.getElementById('filters').contains(e.target)) document.getElementById('filters').zIndex = -2
 })
